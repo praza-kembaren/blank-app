779c3719a271c0943ad51416837248ede6699d16
PKembaren (アルタイル) <prazakembaren@gmail.com>
2024-10-26 15:02:36 +0000

    change

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,13 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-
 st.header("Introduction", divider='rainbow')
 st.write(
     "Hallo, Wellcome to my personal website, hehe"
 )
 
 chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+     np.random.randn(20, 4),
+     columns=['a', 'b', 'c', 'd'])
 
 st.line_chart(chart_data)
