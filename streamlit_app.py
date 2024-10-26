92ba26fa7bccbfbb5d24a2abfff375ca54e5dfcb
PKembaren (アルタイル) <prazakembaren@gmail.com>
2024-10-26 14:44:37 +0000

    change

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+
 
 st.header("Introduction", divider='rainbow')
 st.write(
     "Hallo, Wellcome to my personal website, hehe"
 )
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
