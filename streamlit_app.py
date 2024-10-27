8ca2524d053054f780e5b70bee83a186fade4f6e
PKembaren (アルタイル) <prazakembaren@gmail.com>
2024-10-27 14:20:34 +0000

    change

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,28 @@ import numpy as np
 import pandas as pd
 import math
 import matplotlib.pyplot as plt
+import base64
+
 
 
 # Set up the page
-st.set_page_config(page_title="home", page_icon=":milkyway:")
+st.set_page_config(page_title="home", page_icon=":milky_way:")
 
-st.header("About Me", divider='rainbow')
-st.write(
-    "Hello, I’m Praza, an Atmospheric and Planetary Sciences grad with a big interest in simulations, exoplanets, and using Python to dive into astronomy. I love exploring data to get new insights into how planets form and what’s out there in our universe."
-)
+col1, col2 = st.columns(2)
+
+with col1:
+    st.header("About Me", divider='rainbow')
+    st.write("Hello, I’m Praza, an Atmospheric and Planetary Sciences grad with a big interest in simulations, exoplanets, and using Python to dive into astronomy. I love exploring data to get new insights into how planets form and what’s out there in our universe.")
+
+with col2:
+    file_ = open("/workspaces/blank-app/file/ezgif.com-speed.gif", "rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
+
+    st.markdown(f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+    unsafe_allow_html=True,)
+ 
 
 # # Add social links or contact
 # st.markdown("""
