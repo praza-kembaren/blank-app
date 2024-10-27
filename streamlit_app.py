fe9b9bbbba417a935f3c48a33600e8967b38d0c1
PKembaren (アルタイル) <prazakembaren@gmail.com>
2024-10-27 14:25:41 +0000

    change

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,12 @@ import base64
 
 # Set up the page
 st.set_page_config(page_title="home", page_icon=":milky_way:")
-
+st.header("About Me", divider='rainbow')
 col1, col2 = st.columns(2)
 
 with col1:
-    st.header("About Me", divider='rainbow')
-    st.write("Hello, I’m Praza, an Atmospheric and Planetary Sciences grad with a big interest in simulations, exoplanets, and using Python to dive into astronomy. I love exploring data to get new insights into how planets form and what’s out there in our universe.")
+    st.markdown('<div style="text-align: justify;">Hello, I’m Praza, an Atmospheric and Planetary Sciences grad with a big interest in simulations, exoplanets, and using Python to dive into astronomy. I love exploring data to get new insights into how planets form and what’s out there in our universe.</div>', unsafe_allow_html=True)
+  
 
 with col2:
     file_ = open("/workspaces/blank-app/file/ezgif.com-speed.gif", "rb")
@@ -22,7 +22,7 @@ with col2:
     data_url = base64.b64encode(contents).decode("utf-8")
     file_.close()
 
-    st.markdown(f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+    st.markdown(f'<img src="data:image/gif;base64,{data_url}" width="250" height="250" alt="tco gif">',
     unsafe_allow_html=True,)
  
 
