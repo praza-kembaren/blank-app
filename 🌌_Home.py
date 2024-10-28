d18a84b5e9682f4a516ef950f53b1c56dbf186de
PKembaren (アルタイル) <prazakembaren@gmail.com>
2024-10-28 07:04:17 +0000

    change

--- 🌌_Home.py
+++ 🌌_Home.py
@@ -27,37 +27,3 @@ with col2:
 
     st.markdown(f'<img src="data:image/gif;base64,{data_url}" width="300" height="300" alt="tco gif">',
     unsafe_allow_html=True,)
-
-if "logged_in" not in st.session_state:
-    st.session_state.logged_in = False
-
-def login():
-    if st.button("Log in"):
-        st.session_state.logged_in = True
-        st.rerun()
-
-def logout():
-    if st.button("Log out"):
-        st.session_state.logged_in = False
-        st.rerun()
-
-login_page = st.Page(login, title="Log in", icon=":material/login:")
-logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
-
-cdm = st.Page("/workspaces/blank-app/AstroPython/Comet Destination Map.py", title="Comet Destination Map", icon=":material/dashboard:", default=True)
-ci  = st.Page("Meteo/Cloud Identification.py", title="Clouds Identification", icon=":material/bug_report:")
-esi = st.Page("Course Work/Earth Similarity Index.py", title="Earth Similarity Index", icon=":material/notification_important:")
-
-if st.session_state.logged_in:
-    pg = st.navigation(
-        {
-            "AstroPython": [cdm],
-            "Meteo": [ci],
-            "Course Work": [esi],
-        }
-    )
-else:
-    pg = st.navigation([login_page])
-
-pg.run()
- 
