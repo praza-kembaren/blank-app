573521cdd4dfc53ba63d396e735f044ff894dc30
PKembaren (アルタイル) <prazakembaren@gmail.com>
2024-10-28 02:51:08 +0000

    change

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ with col1:
     
     st.markdown('<div style="text-align: justify;">Hello, I’m Praza, an Atmospheric and Planetary Sciences grad with a big interest in creating N-Body simulations, modeling the trajectories or orbits of small solar system bodies, analyzing their orbital evolution and dynamics, and studying exoplanet systems. This website contains my coursework as a student and a few other small astronomy-related topics.</div>', unsafe_allow_html=True)
     st.subheader("Get in touch on me ", divider='rainbow')
-    
+
     st.markdown("""[LinkedIn](https://www.linkedin.com/in/praza-kembaren) | [GitHub](https://github.com/praza-kembaren) | [Twitter](https://x.com/prazakembaren)""")
     column1, column2, column3 = st.columns(3)
     column1.markdown("[![Title]('https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg')]('https://www.linkedin.com/in/praza-kembaren')")
